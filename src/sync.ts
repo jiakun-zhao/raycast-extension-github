9e2564ad827381d20c6c5007c121438640216765
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-12-21 15:16:15 +0800

    fix: runInBackground

--- src/sync.ts
+++ src/sync.ts
@@ -1,17 +1,18 @@
-import { Toast, showToast, updateCommandMetadata } from '@raycast/api'
+import { LaunchType, Toast, environment, showToast, updateCommandMetadata } from '@raycast/api'
 import { set } from './utils'
 
 export default async function Command() {
+  const notBackground = environment.launchType !== LaunchType.Background
   try {
-    await showToast({ title: 'Fetch followed...', style: Toast.Style.Animated })
+    notBackground && await showToast({ title: 'Fetch followed...', style: Toast.Style.Animated })
     await set('followed')
-    await showToast({ title: 'Fetch repos...', style: Toast.Style.Animated })
+    notBackground && await showToast({ title: 'Fetch repos...', style: Toast.Style.Animated })
     await set('repos')
-    await showToast({ title: 'Fetch stars...', style: Toast.Style.Animated })
+    notBackground && await showToast({ title: 'Fetch stars...', style: Toast.Style.Animated })
     await set('stars')
-    await showToast({ title: 'Success.', style: Toast.Style.Success })
+    notBackground && await showToast({ title: 'Success.', style: Toast.Style.Success })
     await updateCommandMetadata({ subtitle: `Last update: ${new Date().toLocaleString()}` })
   } catch {
-    await showToast({ title: 'Fail.', style: Toast.Style.Failure })
+    notBackground && await showToast({ title: 'Fail.', style: Toast.Style.Failure })
   }
 }
